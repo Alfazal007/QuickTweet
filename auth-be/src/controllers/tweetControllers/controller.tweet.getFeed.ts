8e4cf653b3c58ae25ca3d99a0bc45ce8e9132c42
Alfazal007 <alfazal7777@gmail.com>
2024-10-17 15:26:11 +0530

    feed data added with pagination

---
+++ auth-be/src/controllers/tweetControllers/controller.tweet.getFeed.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from "express";
+import { asyncHandler } from "../../utils/AsyncHandler";
+import { ApiError } from "../../utils/ApiError";
+import { ISSUEWITHDATABASE, SUCCESSFULLYFETCHEDTHEDATA, UNAUTHORIZED } from "../../constants/ReturnTypes";
+import { prisma } from "../../constants/prisma";
+import { ApiResponse } from "../../utils/ApiResponse";
+
+const getFeed = asyncHandler(async(req: Request, res: Response) => {
+    if(!req.user) {
+        return res.status(401).json(new ApiError(401, UNAUTHORIZED, []));
+    }
+    try {
+        let { page } = req.params;
+        if(page == '0') {
+            page = '1';
+        }
+
+        const pageNumber = parseInt(page, 10) || 1;
+        const limitNumber = 10;
+
+        const skip = (pageNumber - 1) * limitNumber;
+        const peopleUserFollows = await prisma.follow.findMany({
+            where: {
+                followerId: {
+                    in: [req.user.id]
+                },
+            },
+            select: {
+                followingId: true
+            }
+        });
+        const peopleUserFollowsArray = peopleUserFollows.map((data) => data.followingId);
+        peopleUserFollowsArray.push(req.user.id);
+        if(peopleUserFollows.length == 0) {
+            return res.status(200).json(new ApiResponse(200, SUCCESSFULLYFETCHEDTHEDATA, {}));
+        }
+        const tweetsForFeed = await prisma.tweet.findMany({
+            where: {
+                creatorId: {
+                    in: peopleUserFollowsArray
+                }
+            },
+            skip,
+            take: limitNumber,
+            select: {
+                creatorId: true,
+                createdAt: true,
+                id: true,
+                picture: true,
+                description: true,
+                replyCount: true,
+                repostCount: true,
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        });
+        return res.status(200).json(new ApiResponse(200, SUCCESSFULLYFETCHEDTHEDATA, tweetsForFeed));
+    } catch(err) {
+        return res.status(400).json(new ApiError(400, ISSUEWITHDATABASE, []));
+    }
+});
+
+export {
+    getFeed
+}
--- auth-be/src/routers/tweet.router.ts
+++ auth-be/src/routers/tweet.router.ts
@@ -5,12 +5,14 @@ import { createTweet } from "../controllers/tweetControllers/controller.tweet.cr
 import { deleteTweetController } from "../controllers/tweetControllers/controller.tweet.delete";
 import { getUserTweets } from "../controllers/tweetControllers/controller.tweet.getUserTweets";
 import { getTweet } from "../controllers/tweetControllers/controller.tweet.getTweet";
+import { getFeed } from "../controllers/tweetControllers/controller.tweet.getFeed";
 const tweetRouter = Router();
 
 tweetRouter.route("/create").post(authMiddleware, upload.single("tweetPic"), createTweet);
 tweetRouter.route("/delete").delete(authMiddleware, deleteTweetController);
 tweetRouter.route("/user/:username/page/:page").get(authMiddleware, getUserTweets);
 tweetRouter.route("/get/tweetId/:tweetId").get(authMiddleware, getTweet);
+tweetRouter.route("/feed/page/:page").get(authMiddleware, getFeed);
 
 export {
    tweetRouter 
